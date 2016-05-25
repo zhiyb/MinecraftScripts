d15be4541e907583b7327c7b4be930ce324f160e
Norman Zhi <normanzyb@gmail.com>
2016-05-25 22:22:24 +0100

    Script for updating to latest version of minecraft

--- start.sh
+++ start.sh
@@ -1,4 +1,18 @@
-#!/bin/sh
-JAR=minecraft_server.1.8.7.jar
-JAR=minecraft_server.15w34a.jar
-java -Xincgc -Xmx1024m -jar "$JAR" nogui
+#!/bin/bash
+java="java -Xmx6G"
+args="nogui"
+restart=false
+
+update=true
+infofile=latest.txt
+
+while :; do
+	[ "$update" == "true" ] && ./update.sh "$infofile"
+	[ -e "$infofile" ] && file="$(<$infofile)"
+	[ -z "$file" ] && echo -e "\e[1;31mCannot locate executable file from $infofile\e[0m" && read -s && exit 1
+
+	echo -e "\e[1;33mStarting $file...\e[0m"
+	$java -jar ./"$file" $args
+	[ "$restart" != "true" ] && read -s && exit 0
+	sleep 5
+done
---
+++ update.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# snapshot or release
+type=snapshot
+manifest=https://launchermeta.mojang.com/mc/game/version_manifest.json
+
+infofile="$1"
+[ -z "$infofile" ] && infofile=latest.txt
+
+get()
+{
+	curl -s $@
+}
+
+download()
+{
+	curl --progress-bar -o "$2" -L "$1"
+	#aria2c --daemon=false --enable-rpc=false -c -o "$2" "$1"
+}
+
+echo "Fetching manifest..."
+manifest="$(get "$manifest")"
+
+version="$(echo "$manifest" | jq -r ".latest.$type")"
+info="$(echo "$manifest" | jq -r ".versions[] | select(.id | contains(\"$version\"))")"
+time="$(echo "$info" | jq -r ".releaseTime")"
+url="$(echo "$info" | jq -r ".url")"
+echo "Latest $type: $version ($time)"
+
+file="$version.jar"
+if [ -e "$file" ]; then
+	echo -e "File \e[1;32m$file\e[0m already exists"
+else
+	echo "Fetching version metadata..."
+	meta="$(get "$url")"
+
+	server="$(echo "$meta" | jq -r ".downloads.server")"
+	url="$(echo "$server" | jq -r ".url")"
+
+	echo -e "Downloading \e[1;32m$file\e[0m from: \e[1;33m$url\e[0m"
+	if ! download "$url" "$file"; then
+		echo "\e[1;31mUpdate failed!\e[0m"
+		exit 1
+	fi
+fi
+
+echo "$file" > "$infofile"
+exit 0
