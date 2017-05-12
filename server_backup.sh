836e8e98c8b992baad8a52d505fcd01f570cc7c4
Yubo Zhi <normanzyb@gmail.com>
2017-05-12 21:36:05 +0100

    Backup only when players online

--- server_backup.sh
+++ server_backup.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 [ ! -e "server.conf" ] && echo "server.conf not found!" && exit 1
 . server.conf
@@ -6,18 +6,27 @@
 suffix=.tar.bz2
 pattern="${prefix}[0-9]\{8\}-[0-9]\{6\}\.tar\.bz2\$"
 
-function doBackup
+online()
+{
+	while :; do
+		num="$(netstat -nt | grep -F $port | wc -l)"
+		(($num != 0)) && echo "$num connections." && break
+		sleep 30s
+	done
+}
+
+doBackup()
 {
 	echo -n "Backing up: "
 	screen -S $screen -p 0 -X stuff 'say Backup started, entering read-only mode...\nsave-off\nsave-all\n'
-	sleep 3s
+	sleep 6s
 	echo -n "$1... "
 	tar jcf "$bakfolder/$1" "$source"
 	echo "Done."
 	screen -S $screen -p 0 -X stuff 'save-on\nsay Backup done.\n'
 }
 
-function tidyBackups
+tidyBackups()
 {
 	remove="$(find -L "$bakfolder" -type f -name "${prefix}*-*${suffix}" 2> /dev/null | grep "$pattern" | sort | head -n -$baknum | xargs)"
 	[ -z "$remove" ] && return
@@ -29,9 +38,10 @@ declare -a backups
 mkdir -p "$bakfolder"
 
 while :; do
+	online
+	sleep ${baksleep}
 	filename="${prefix}$(date +%Y%m%d-%H%M%S)${suffix}"
 	doBackup "$filename"
 	tidyBackups
-	sleep ${baksleep}
 done
 exit
